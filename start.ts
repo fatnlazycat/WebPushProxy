70e33bfac4b7230523f7f6a7f9c687e9f7aee9aa
Dmitriy Kosyakov <fatnlazycat@gmail.com>
2022-02-23 14:54:16 +0200

    check_suite

--- start.ts
+++ start.ts
@@ -1,9 +1,15 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 
 const requestListener = function (req: IncomingMessage, res: ServerResponse) {
-  console.log("request", req.method, req.headers);
+  console.log('request', req.url, req.headers);
+  switch (req.url) {
+    case '/': 
+      if (req.method === 'POST' && req.headers['x-github-event'] === 'check_suite') {
+        console.log('got it!');
+      }; break;
+  }
   res.writeHead(200);
-  res.end('Hello, World 2!');
+  res.end('Hello, World 3!');
 }
 
 const server = http.createServer(requestListener);
