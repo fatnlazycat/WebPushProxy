efb5dd95c2377c872220f987e3938a29a6641c21
Dmitriy Kosyakov <fatnlazycat@gmail.com>
2022-02-23 14:43:44 +0200

    console

--- start.ts
+++ start.ts
@@ -1,6 +1,7 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 
 const requestListener = function (req: IncomingMessage, res: ServerResponse) {
+  console.log("request", req.method, req.headers);
   res.writeHead(200);
   res.end('Hello, World 2!');
 }
