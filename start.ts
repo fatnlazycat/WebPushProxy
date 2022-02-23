db3111794c20e89ccf82127bed03e38088f066f1
Dmitriy Kosyakov <fatnlazycat@gmail.com>
2022-02-23 16:56:14 +0200

    tmp

--- start.ts
+++ start.ts
@@ -10,46 +10,48 @@ const requestListener = function (req: IncomingMessage, res: ServerResponse) {
   })
   req.on('end', () => {
     console.log('payload', dataMain);
+
+    switch (req.url) {
+      case '/': 
+        if (req.method === 'POST' && req.headers['x-github-event'] === 'check_suite') {
+          console.log('got it!');
+          const b = JSON.parse(dataMain);
+          const a = b.check_suite ? b.check_suite.head_sha : b.check_run.head_sha
+          const data = JSON.stringify({
+            name: 'Buy the milk',
+            head_sha: a,
+          });
+          
+          const options = {
+            hostname: 'api.github.com',
+            path: '/repos/fatnlazycat/githubToArgoProxy/check-runs',
+            method: 'POST',
+            headers: {
+              'Accept': 'application/vnd.github.v3+json',
+            }
+          }
+          
+          const r = http.request(options, resp => {
+            console.log(`statusCode: ${res.statusCode}`)
+          
+            resp.on('data', d => {
+              process.stdout.write(d)
+            })
+          })
+          
+          r.on('error', error => {
+            console.error(error)
+          })
+          
+          r.write(data)
+          r.end()
+          return;
+        }; 
+    }
+    res.writeHead(200).end('Hello, World 4!');
   });
 
-  switch (req.url) {
-    case '/': 
-      if (req.method === 'POST' && req.headers['x-github-event'] === 'check_suite') {
-        console.log('got it!');
-        const b = JSON.parse(dataMain);
-        const a = b.check_suite ? b.check_suite.head_sha : b.check_run.head_sha
-        const data = JSON.stringify({
-          name: 'Buy the milk',
-          head_sha: a,
-        });
-        
-        const options = {
-          hostname: 'api.github.com',
-          path: '/repos/fatnlazycat/githubToArgoProxy/check-runs',
-          method: 'POST',
-          headers: {
-            'Accept': 'application/vnd.github.v3+json',
-          }
-        }
-        
-        const r = http.request(options, resp => {
-          console.log(`statusCode: ${res.statusCode}`)
-        
-          resp.on('data', d => {
-            process.stdout.write(d)
-          })
-        })
-        
-        r.on('error', error => {
-          console.error(error)
-        })
-        
-        r.write(data)
-        r.end()
-        return;
-      }; 
-  }
-  res.writeHead(200).end('Hello, World 4!');
+  
 }
 
 const server = http.createServer(requestListener);
